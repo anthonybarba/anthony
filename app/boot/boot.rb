73b7a256db92202877c50c2cbde05640732f60b9
EC2 Default User <ec2-user@ip-172-31-45-156.eu-west-1.compute.internal>
2018-07-21 15:06:57 +0000

    tmtc

--- app/boot/boot.rb
+++ app/boot/boot.rb
@@ -6,9 +6,11 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
     if message.text.include? "Bonjour"
-       message.reply(text: "Bonjour Monsieur")
-   elsif message.text.include? "Bonne nuit"
+        message.reply(text: "Bonjour Monsieur")
+    if message.text.include? "Bonne nuit"
         message.reply(text: "Dors bien")
+    if message.text.include? "comment vas tu ?"
+        message.reply(text: "très bien et vous")
     else
         message.reply(text: 'Reponse par defaut')
      end
